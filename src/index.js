7963c21f9b00e562c37bbe7d0bc91497d758f711
Yauheni-Baranski <yauheni.baranski@gmail.com>
2021-03-03 04:38:55 +0300

    fix:average function fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    let minValue = 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        a < b ? a : b;
+        if (a < b) {
+            return (minValue = a);
+        }
+        return (minValue = b);
+    });
+    return minValue;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let maxValue = 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        a > b ? a : b;
+        if (a > b) {
+            return (maxValue = a);
+        }
+        return (maxValue = b);
+    });
+    return maxValue;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let avgValue = 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+    avgValue = array.reduce(function (a, b) {
+        return a + b;
+    });
+    return avgValue / array.length;
+};
